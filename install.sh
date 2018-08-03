240454c1a112334b2f0a72031ff6b7dbf67c001a
Simen Berge <bergesimen@gmail.com>
2018-08-03 13:46:56 +0200

    wip

--- install.sh
+++ install.sh
@@ -11,3 +11,7 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 # update dotfiles itself
 if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+
+# Bunch of symlinks
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
