f9b09f5231f40c099b74d71b43ad120eb9c837c7
Simen Berge <bergesimen@gmail.com>
2018-08-03 13:17:12 +0200

    init commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Get current dir
+export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
+DOTFILES_EXTRA_DIR="$HOME/.extra"
+
+# make utilities available
+PATH="$DOTFILES_DIR/bin:$PATH"
+
+# update dotfiles itself
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
