a3fc9837eb5fcab61b2dffc5e1eb265fd9737816
Simen Berge <bergesimen@gmail.com>
2019-07-15 21:26:29 +0200

    updated zsh installation

--- install/zsh.sh
+++ install/zsh.sh
@@ -7,9 +7,6 @@ if is-macos -o; then
   brew install zsh
   # use zsh installed by brew
   sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
-else
-  sudo apt install zsh
-  chsh -s /usr/bin/zsh
 fi
 
 if ! is-executable zsh; then
@@ -20,12 +17,16 @@ fi
 echo "installing oh-my-zsh"
 export ZSH="$HOME/.oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# add oxide theme
-ln -s $DOTFILES/.oh-my-zsh/themes/oxide.zsh-theme $ZSH/themes
-
 echo "installing fast-syntax-highlighting"
 git clone https://github.com/zdharma/fast-syntax-highlighting.git "~/.oh-my-zsh/custom/plugins"
 
+# install theme
+if ! is-executable curl; then
+  echo "missing curl, failed getting zsh theme"
+else
+  mkdir -p  ~/.oh-my-zsh/custom/themes
+  curl https://raw.githubusercontent.com/win0err/aphrodite-terminal-theme/master/aphrodite.zsh-theme --output ~/.oh-my-zsh/custom/themes/aphrodite.zsh-theme
+fi
 
 if ! is-executable fzf --help; then
   echo "installing fzf"
