354eae46241e26e4fa4a7a6e73c3aaae9d0798ea
Simen Berge <bergesimen@gmail.com>
2021-11-23 09:47:49 +0100

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,32 +6,10 @@ if type -q $pnpm
 end
 
 # Fish syntax highlighting
-# set -g fish_color_autosuggestion '555'  'brblack'
 set -g fish_color_autosuggestion abb2bf 145 7
-# set -g fish_color_cancel -r
-# set -g fish_color_command --bold
-# set -g fish_color_comment red
-# set -g fish_color_cwd green
-# set -g fish_color_cwd_root red
-# set -g fish_color_end brmagenta
-# set -g fish_color_error brred
-# set -g fish_color_escape 'bryellow'  '--bold'
-# set -g fish_color_history_current --bold
-# set -g fish_color_host normal
-# set -g fish_color_match --background=brblue
 set -g fish_color_command  699959 71  2
 set -g fish_color_match    699959 71  2
-# set -g fish_color_normal normal
-# set -g fish_color_operator bryellow
-# set -g fish_color_param cyan
-# set -g fish_color_quote yellow
-# set -g fish_color_redirection brblue
-# set -g fish_color_search_match 'bryellow'  '--background=brblack'
-# set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
-# set -g fish_color_user brgreen
-# set -g fish_color_valid_path --underline
 
-# Install Starship
 if type -q $starship
   starship init fish | source
 end
@@ -39,3 +17,5 @@ end
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 set MANPATH $NPM_PACKAGES/share/man $MANPATH
+
+set PATH $PATH /home/sib/go/bin
