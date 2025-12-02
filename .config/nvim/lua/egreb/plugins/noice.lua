7841bf415648c79c9a7adf77e30ef9e0585ea00e
Simen Berge <bergesimen@gmail.com>
2025-12-02 07:15:43 +0100

    update noice config

--- .config/nvim/lua/egreb/plugins/noice.lua
+++ .config/nvim/lua/egreb/plugins/noice.lua
@@ -2,32 +2,28 @@ return {
 	'folke/noice.nvim',
 	event = 'VeryLazy',
 	opts = {
-		routes = {
-			{
-				filter = {
-					event = 'msg_show',
-					any = {
-						{ find = '%d+L, %d+B' },
-						{ find = '; after #%d+' },
-						{ find = '; before #%d+' },
-					},
-				},
-				view = 'mini',
-			},
-		},
-		presets = {
-			bottom_search = true,
-			command_palette = true,
-			long_message_to_split = true,
-		},
+		-- add any options here
 	},
-	config = function(_, opts)
-		-- HACK: noice shows messages from before it was enabled,
-		-- but this is not ideal when Lazy is installing plugins,
-		-- so clear the messages in this case.
-		if vim.o.filetype == 'lazy' then
-			vim.cmd [[messages clear]]
-		end
-		require('noice').setup(opts)
+	dependencies = {
+		'MunifTanjim/nui.nvim',
+		'rcarriga/nvim-notify',
+	},
+	config = function()
+		require('noice').setup {
+			lsp = {
+				override = {
+					['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+					['vim.lsp.util.stylize_markdown'] = true,
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false,   -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		}
 	end,
 }
