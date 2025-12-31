c0ed73ae7cf971e6556a73a9b214a3d011ca6f36
Simen Berge <bergesimen@gmail.com>
2025-12-31 10:10:29 +0100

    change colorscheme

--- .config/nvim/lua/egreb/plugins/colorscheme.lua
+++ .config/nvim/lua/egreb/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
 return {
-	'rebelot/kanagawa.nvim',
-	lazy = false,  -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other plugins
+	'mcncl/alabaster.nvim',
+	lazy = false,
+	priority = 1000,
 	config = function()
-		vim.cmd 'colorscheme kanagawa'
+		require('alabaster').setup {
+			style = 'dark', -- "light" or "dark"
+		}
+		vim.cmd.colorscheme 'alabaster'
 	end,
 }
