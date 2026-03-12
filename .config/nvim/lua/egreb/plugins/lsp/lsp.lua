5b0406d3c32db5d4190c30a0947eb6183ee36419
Simen Berge <bergesimen@gmail.com>
2026-03-12 17:29:45 +0100

    chore: lsp styling

--- .config/nvim/lua/egreb/plugins/lsp/lsp.lua
+++ .config/nvim/lua/egreb/plugins/lsp/lsp.lua
@@ -9,31 +9,35 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
+		keymap = {
+			preset = 'default',
+			['<CR>'] = { 'accept', 'fallback' },
+		},
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = 'mono',
 		},
 		fuzzy = { implementation = 'prefer_rust_with_warning' },
-		signature = { window = { border = 'padded' } },
+		signature = { window = { border = 'rounded' } },
 		completion = {
 			accept = { auto_brackets = { enabled = false } },
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 250,
 				window = {
-					border = 'padded',
+					border = 'rounded',
 				},
 			},
 			menu = {
-				border = 'padded',
+				border = 'rounded',
 				draw = {
-					gap = 2,
-					columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind' } },
+					gap = 1,
+					columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'kind' } },
 				},
 			},
 			-- Display a preview of the selected item on the current line
-			ghost_text = { enabled = false },
+			ghost_text = { enabled = true },
 		},
 		sources = {
 			default = { 'lsp', 'path', 'buffer' },
