0ddb6b9f34dfaef60c268afd2022093e46e20e77
Simen Berge <bergesimen@gmail.com>
2026-06-07 07:01:36 +0200

    catpuccin all the things

--- .config/nvim/lua/egreb/plugins/colorscheme.lua
+++ .config/nvim/lua/egreb/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	'serhez/teide.nvim',
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd [[colorscheme teide-dimmed]]
-	end,
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    vim.cmd 'colorscheme catppuccin-frappe'
+  end,
 }
